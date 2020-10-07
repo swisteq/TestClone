776abe20bdc136c8f04148343ad859b24c7cedd7
Matt018 <mateusz.hein@edu.eukat.pl>
2020-10-07 21:57:29 +0200

    testcommit and push

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SklepTest
+{
+    class TShirt
+    {
+        int size;
+        string material;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
